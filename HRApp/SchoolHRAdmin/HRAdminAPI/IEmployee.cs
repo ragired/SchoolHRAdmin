94fd1ed5a4c68efc3ff258f002b3835e5156df9e
ragired <ragiredd@gmail.com>
2025-03-23 23:24:34 +0530

    School HR Admin project
    introduces the concept of
    Interface Segregation
    Dependency Inversion
    Single Responsibility
    ( S + I + D ) out of SOLID

---
+++ HRApp/SchoolHRAdmin/HRAdminAPI/IEmployee.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Interface definition for an Employee
+ * This realizes the SOLID Goal Dependency Inversion
+ * The base class can have a list of IEmployee objects 
+ * That way, the base class is not dependent on changing each time the derived class is changed / extended
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdminAPI
+{
+    public interface IEmployee
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal Salary   { get; set; }
+
+
+    }
+}
---
+++ HRApp/SchoolHRAdmin/SchoolHRAdmin/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+ *  Program.cs
+ *  
+ *  School HR Records of employees
+ *  
+ */
+using HRAdminAPI;
+
+
+namespace SchoolHRAdmin;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal totalSalary = 0;
+        List<IEmployee> employeeList = new List<IEmployee>();
+        Console.WriteLine("Employee Salary Data");
+        SeedData(employeeList);
+        /* foreach(var employee in employeeList)
+         {
+             totalSalary += employee.Salary;
+         }
+         Console.WriteLine($"Total Salary incl Bonus {totalSalary}");*/
+
+        Console.WriteLine($"Total Salary incl Bonus {employeeList.Sum(e => e.Salary)}");
+    }
+
+    public static void SeedData(List<IEmployee> employees)
+    {
+        IEmployee headOFDept = new HeadOfDept
+        {
+            Id = 3,
+            FirstName = "Brenda",
+            LastName = "Br",
+            Salary = 30000
+        };
+        employees.Add(headOFDept);
+
+        IEmployee headMaster = new HeadMaster
+        {
+            Id = 5,
+            FirstName = "Max",
+            LastName = "Mu",
+            Salary = 25000
+        };
+        employees.Add(headMaster);
+
+        IEmployee teacher = new Teacher
+        {
+            Id = 7,
+            FirstName = "Paula",
+            LastName = "Pr",
+            Salary = 15000
+        };
+        employees.Add(teacher);
+
+        IEmployee dHM = new DeputyHeadMaster
+        {
+            Id = 9,
+            FirstName = "Simon",
+            LastName = "Si",
+            Salary = 18000
+        };
+        employees.Add(dHM);
+
+    }
+}
+
+
+public class Teacher : EmployeeBase
+{
+    public override decimal Salary { get => base.Salary + base.Salary*0.03m; }
+
+}
+public class HeadMaster : EmployeeBase
+{
+    public override decimal Salary { get => base.Salary + base.Salary * 0.02m; }
+
+}
+public class DeputyHeadMaster : EmployeeBase
+{
+    public override decimal Salary { get => base.Salary + base.Salary * 0.05m; }
+
+}
+public class HeadOfDept : EmployeeBase
+{
+    public override decimal Salary { get => base.Salary + base.Salary * 0.015m; }
+
+}
